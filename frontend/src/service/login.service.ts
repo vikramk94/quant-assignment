358cc43df3aec24296f6ea8ed6dc0907534984a4
vikramk94 <vikramvicky907@gmail.com>
2025-08-18 12:13:40 +0530

    feat: add token management functions for login service

--- frontend/src/service/login.service.ts
+++ frontend/src/service/login.service.ts
@@ -1,5 +1,18 @@
 import axiosObj from './axios'
 
+export const clearToken = () => {
+  localStorage.removeItem('ACCESS_TOKEN')
+}
+
+export const storeToken = (token: string) => {
+  localStorage.setItem('ACCESS_TOKEN', token)
+}
+
+export const getAccessToken = () => {
+  return localStorage.getItem('ACCESS_TOKEN')
+}
+
+
 export const login = async (username: string, password: string) => {
   try {
     const response = await axiosObj.post('/login', { username, password })
